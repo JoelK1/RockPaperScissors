42932ae6d8795831eaa2bba925c5450e7d022d23
JoelK1 <bigboppakelly@gmail.com>
2022-11-13 14:46:31 +1100

    index.html java.js and style.css after first exercise

--- java.js
+++ java.js
@@ -1,3 +1,6 @@
+
+function playRound() {
+    
 let randomNumber = Math.floor(Math.random()*3)+1;
 
 var getComputerChoice = "Rock";
@@ -15,31 +18,46 @@ if(randomNumber === 1) {
     console.log(getComputerChoice);
 }
 
-var getComputerChoice = getComputerChoice.toLowerCase();
+const computerSelection = getComputerChoice.toLowerCase();
 
-var playerSelection = prompt("Select Rock, Paper or Scissors, GLHF");
+let getPlayerChoice = prompt("Select Rock, Paper or Scissors, GLHF");
 
-var playerSelection = playerSelection.toLowerCase();
+let playerSelection = getPlayerChoice.toLowerCase();
 
-console.log(playerSelection)
 
-console.log(getComputerChoice)
-
-if(playerSelection === "rock" && getComputerChoice === "paper") {
+if(playerSelection === "rock" && computerSelection === "paper") {
     console.log("You Lose! Paper beats Rock");
-}    else if(playerSelection === "rock" && getComputerChoice === "scissors") {
+}    else if(playerSelection === "rock" && computerSelection === "scissors") {
     console.log("You Win! Rock beats Scissors");
 }
-    else if(playerSelection === "paper" && getComputerChoice === "rock") {
+    else if(playerSelection === "paper" && computerSelection === "rock") {
     console.log("You Win! Paper beats Rock");
-}    else if(playerSelection === "paper" && getComputerChoice === "scissors") {
+}    else if(playerSelection === "paper" && computerSelection === "scissors") {
     console.log("You Lose! Scissors beats Paper");
 }
-    else if(playerSelection === "scissors" && getComputerChoice === "rock") {
+    else if(playerSelection === "scissors" && computerSelection === "rock") {
     console.log("You Lose! Rock beats Scissors");
-}    else if(playerSelection === "scissors" && getComputerChoice === "paper") {
+}    else if(playerSelection === "scissors" && computerSelection === "paper") {
     console.log("You Win! Scissors beats Paper");
 }
     else {
-        console.log("Tie or Invalid Selection! Please guess again");
+    console.log("Tie or Invalid Selection! Please guess again");
 }
+}
+
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        if(i <= 5) {
+            playRound()
+        }
+        else {
+            console.log("Game Over")
+        }
+    }
+
+}
+
+game()
+
